d3d1dc11f6e746731d5aac47f5c31e0bcedb444d
abarre <anthony.barre87@gmail.com>
2016-10-04 15:26:39 +0200

    Change Interface name

--- files/FirstReboot.ps1
+++ files/FirstReboot.ps1
@@ -1,7 +1,7 @@
 Start-Sleep -s 60
 
 $InstallDir = "C:\webpagetest"
-$dummynet = Get-NetAdapterBinding -Name public*
+$dummynet = Get-NetAdapterBinding -Name Ethernet
 
 If ($dummynet.ComponentID -eq "ipfw+dummynet") {
   Write-Output "Already Enabled."
@@ -9,6 +9,5 @@ If ($dummynet.ComponentID -eq "ipfw+dummynet") {
   Import-Certificate -FilePath C:\wpt-agent\WPOFoundation.cer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
   cd $InstallDir
   .\mindinst.exe C:\wpt-agent\dummynet\64bit\netipfw.inf -i -s
-  Enable-NetAdapterBinding -Name public0 -DisplayName ipfw+dummynet
-  Enable-NetAdapterBinding -Name private0 -DisplayName ipfw+dummynet
+  Enable-NetAdapterBinding -Name Ethernet -DisplayName ipfw+dummynet
 }
